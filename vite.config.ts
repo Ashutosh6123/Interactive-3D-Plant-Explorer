83acb7a5fd538f4aca89c461a8703bf720b2021c
Ashutosh Shukla <ashu6d2@gmail.com>
2025-05-20 01:25:49 +0530

    Update vite-config and package-json for deployment on github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/Interactive-3D-Plant-Explorer/',
   server: {
     host: "::",
     port: 8080,
